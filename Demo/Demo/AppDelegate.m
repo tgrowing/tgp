9e4793540fa56551f77b509eb52346f77dfa0020
yiqiwang <yiqiwang@tencent.com>
2021-03-10 10:29:55 +0800

    Update framework and log

--- Demo/Demo/AppDelegate.m
+++ Demo/Demo/AppDelegate.m
@@ -30,18 +30,21 @@
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
 
+        // log level：1 fetal 2 error 3 warn 4 info 5 debug 10 all
         [BeaconBaseInterface setLogLevel:10];
 
+        // 应用唯一key 必须设置
         NSString *appKey = [[NSUserDefaults standardUserDefaults] objectForKey:@"appkey"];
         [BeaconBaseInterface setAppKey:appKey];
         
-        // 私有化必须设置
+        // 私有化必须设置，默认为内部联调域名
         NSString *uploadURL = [[NSUserDefaults standardUserDefaults] objectForKey:@"uploadurl"];
         [BeaconBaseInterface setUploadURL:uploadURL];
 
         NSString *stratergyURL = [[NSUserDefaults standardUserDefaults] objectForKey:@"stratergyurl"];
         [BeaconBaseInterface setStratergyURL:stratergyURL];
         
+        // 启用
         [BeaconBaseInterface enableAnalytics:nil gatewayIP:nil];
     });
 }
